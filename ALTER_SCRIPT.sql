5e6715deb2c039783a7a700c1345fbca52822491
avankoppen <arnold@vankoppen.com>
2015-01-22 15:59:00 -0500

    removed oracle code

--- ALTER_SCRIPT.sql
+++
@@ -1,6 +0,0 @@
-alter table xxarg.xxarg_hr_401k_dem_man_stg add
-  (brand varchar2(6),
-    payroll_name varchar2(10) );
-
- 
-
--- Altertbl_XXARG_HR_UBSTAXIMP_IFACE_ManualTaxOverride.sql
+++
@@ -1,2 +0,0 @@
-
-alter table xxarg.XXARG_HR_UBSTAXIMP_IFACE add manual_tax_override VARCHAR2(1);
